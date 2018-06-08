7013dfefab3a063b47136c32b3d0e2a1a4068dc3
Shakeel Butt <shakeelb@google.com>
2018-06-08 10:11:37 +1000

    mm: fix race between kmem_cache destroy, create and deactivate
    The memcg kmem cache creation and deactivation (SLUB only) is
    asynchronous.  If a root kmem cache is destroyed whose memcg cache is in
    the process of creation or deactivation, the kernel may crash.
    
    Example of one such crash:
    	general protection fault: 0000 [#1] SMP PTI
    	CPU: 1 PID: 1721 Comm: kworker/14:1 Not tainted 4.17.0-smp
    	...
    	Workqueue: memcg_kmem_cache kmemcg_deactivate_workfn
    	RIP: 0010:has_cpu_slab
    	...
    	Call Trace:
    	? on_each_cpu_cond
    	__kmem_cache_shrink
    	kmemcg_cache_deact_after_rcu
    	kmemcg_deactivate_workfn
    	process_one_work
    	worker_thread
    	kthread
    	ret_from_fork+0x35/0x40
    
    To fix this race, on root kmem cache destruction, mark the cache as dying
    and flush the workqueue used for memcg kmem cache creation and
    deactivation.
    
    Link: http://lkml.kernel.org/r/20180521174116.171846-1-shakeelb@google.com
    Signed-off-by: Shakeel Butt <shakeelb@google.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Greg Thelen <gthelen@google.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/slab.c
+++ mm/slab.c
@@ -1881,8 +1881,8 @@ __kmem_cache_alias(const char *name, unsigned int size, unsigned int align,
 	struct kmem_cache *cachep;
 
 	cachep = find_mergeable(size, align, flags, name, ctor);
-	if (cachep) {
-		cachep->refcount++;
+	if (cachep && kmem_cache_tryget(cachep)) {
+		cachep->alias_count++;
 
 		/*
 		 * Adjust the object sizes so that we clear
--- mm/slub.c
+++ mm/slub.c
@@ -4231,8 +4231,8 @@ __kmem_cache_alias(const char *name, unsigned int size, unsigned int align,
 	struct kmem_cache *s, *c;
 
 	s = find_mergeable(size, align, flags, name, ctor);
-	if (s) {
-		s->refcount++;
+	if (s && kmem_cache_tryget(s)) {
+		s->alias_count++;
 
 		/*
 		 * Adjust the object sizes so that we clear
@@ -4247,7 +4247,8 @@ __kmem_cache_alias(const char *name, unsigned int size, unsigned int align,
 		}
 
 		if (sysfs_slab_alias(s, name)) {
-			s->refcount--;
+			s->alias_count--;
+			kmem_cache_put_locked(s);
 			s = NULL;
 		}
 	}
@@ -4970,7 +4971,8 @@ SLAB_ATTR_RO(ctor);
 
 static ssize_t aliases_show(struct kmem_cache *s, char *buf)
 {
-	return sprintf(buf, "%d\n", s->refcount < 0 ? 0 : s->refcount - 1);
+	return sprintf(buf, "%d\n",
+		       s->alias_count < 0 ? 0 : s->alias_count - 1);
 }
 SLAB_ATTR_RO(aliases);
 
@@ -5117,7 +5119,7 @@ static ssize_t trace_store(struct kmem_cache *s, const char *buf,
 	 * as well as cause other issues like converting a mergeable
 	 * cache into an umergeable one.
 	 */
-	if (s->refcount > 1)
+	if (s->alias_count > 1)
 		return -EINVAL;
 
 	s->flags &= ~SLAB_TRACE;
@@ -5235,7 +5237,7 @@ static ssize_t failslab_show(struct kmem_cache *s, char *buf)
 static ssize_t failslab_store(struct kmem_cache *s, const char *buf,
 							size_t length)
 {
-	if (s->refcount > 1)
+	if (s->alias_count > 1)
 		return -EINVAL;
 
 	s->flags &= ~SLAB_FAILSLAB;
