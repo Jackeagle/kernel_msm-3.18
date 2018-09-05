99c88f4274a7343d1623313c608a35450d6c966b
mandrew <mandrew@codeaurora.org>
2018-09-05 09:46:26 +0530

    Revert "bridge: Pass serial number to Linux bridge port lookup"
    This reverts commit 8918ee57c800c9b2d2fa586f0955f8da3d557e78.
    
    Change-Id: I5b1b006dbedc1e82c9ade8cda33c831d82a1220a
    Signed-off-by: mandrew <mandrew@codeaurora.org>

--- net/bridge/br_if.c
+++ net/bridge/br_if.c
@@ -485,7 +485,7 @@ void __net_exit br_net_exit(struct net *net)
  *       the mac has no associated port
  */
 struct net_device *br_port_dev_get(struct net_device *dev, unsigned char *addr,
-	struct sk_buff *skb, unsigned int cookie)
+	struct sk_buff *skb)
 {
 	struct net_bridge_fdb_entry *fdbe;
 	struct net_bridge *br;
@@ -503,8 +503,7 @@ struct net_device *br_port_dev_get(struct net_device *dev, unsigned char *addr,
 		port_dev_get_hook = rcu_dereference(br_port_dev_get_hook);
 		if (port_dev_get_hook) {
 			struct net_bridge_port *pdst =
-				__br_get(port_dev_get_hook, NULL, dev, skb,
-					 addr, cookie);
+				__br_get(port_dev_get_hook, NULL, dev, skb);
 			if (pdst) {
 				dev_hold(pdst->dev);
 				netdev = pdst->dev;
