96469a554c374e6fb4cac102d6b4d14472492690
Jens Axboe <axboe@kernel.dk>
2019-01-09 09:39:42 -0700

    io_uring: add io_uring_event cache hit information
    Add hint on whether a read was served out of the page cache, or if it
    hit media. This is useful for buffered async IO, O_DIRECT reads would
    never have this set (for obvious reasons).
    
    If the read hit page cache, cqe->flags will have IOCQE_FLAG_CACHEHIT
    set.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/uapi/linux/io_uring.h
+++ include/uapi/linux/io_uring.h
@@ -70,6 +70,11 @@ struct io_uring_cqe {
 };
 
 /*
+ * io_uring_event->flags
+ */
+#define IOCQE_FLAG_CACHEHIT	(1U << 0)	/* IO did not hit media */
+
+/*
  * Magic offsets for the application to mmap the data it needs
  */
 #define IORING_OFF_SQ_RING		0ULL
