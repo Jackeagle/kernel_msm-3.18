042aa9382be2d722564df51fbde3894a660a7b13
Anand Jain <anand.jain@oracle.com>
2019-03-01 12:34:54 +0800

    btrfs: don't create transaction in btrfs_setxattr
    The patches
    
    	btrfs: start transaction in btrfs_xattr_handler_set
    	btrfs: start transaction in btrfs_set_acl
    	btrfs: start transaction in btrfs_set_prop_trans
    
    brought changes so that btrfs_setxattr() argument trans is never NULL, so
    delete the part of the code when trans is NULL. All callers are supposed
    to start the transaction.
    
    Signed-off-by: Anand Jain <anand.jain@oracle.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/xattr.c
+++ fs/btrfs/xattr.c
@@ -225,30 +225,12 @@ int btrfs_setxattr(struct btrfs_trans_handle *trans,
 		     const void *value, size_t size, int flags)
 {
 	struct btrfs_root *root = BTRFS_I(inode)->root;
-	int ret;
 
 	if (btrfs_root_readonly(root))
 		return -EROFS;
 
-	if (trans)
-		return do_setxattr(trans, inode, name, value, size, flags);
-
-	trans = btrfs_start_transaction(root, 2);
-	if (IS_ERR(trans))
-		return PTR_ERR(trans);
-
-	ret = do_setxattr(trans, inode, name, value, size, flags);
-	if (ret)
-		goto out;
-
-	inode_inc_iversion(inode);
-	inode->i_ctime = current_time(inode);
-	set_bit(BTRFS_INODE_COPY_EVERYTHING, &BTRFS_I(inode)->runtime_flags);
-	ret = btrfs_update_inode(trans, root, inode);
-	BUG_ON(ret);
-out:
-	btrfs_end_transaction(trans);
-	return ret;
+	ASSERT(trans);
+	return do_setxattr(trans, inode, name, value, size, flags);
 }
 
 ssize_t btrfs_listxattr(struct dentry *dentry, char *buffer, size_t size)
