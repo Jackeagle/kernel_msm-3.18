04b07f111cee21d5e20b166e6aea00cdb22142a3
Vivek Goyal <vgoyal@redhat.com>
2019-03-06 16:51:39 -0500

    fuse: export fuse_send_init_request()
    This will be used by virtio-fs to send init request to fuse server after
    initialization of virt queues.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -139,6 +139,7 @@ void fuse_request_free(struct fuse_req *req)
 	fuse_req_pages_free(req);
 	kmem_cache_free(fuse_req_cachep, req);
 }
+EXPORT_SYMBOL_GPL(fuse_request_free);
 
 void __fuse_get_request(struct fuse_req *req)
 {
