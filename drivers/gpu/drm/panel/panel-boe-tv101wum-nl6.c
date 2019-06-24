059664664342080b6253d57305f23957121ca0bd
Jitao Shi <jitao.shi@mediatek.com>
2019-06-24 16:00:01 +0800

    FROMLIST: drm/panel: support for auo, kd101n80-45na wuxga dsi video mode panel
    [rebase54(mka): squashed with FIXUP patches]
    
    Auo,kd101n80-45na's connector is same as boe,tv101wum-nl6.
    The most codes can be reuse.
    So auo,kd101n80-45na and boe,tv101wum-nl6 use one driver file.
    Add the different parts in driver data.
    
    Signed-off-by: Jitao Shi <jitao.shi@mediatek.com>
    (am from https://patchwork.kernel.org/patch/11012583/)
    (also found at https://lkml.kernel.org/r/20190624080001.67222-3-jitao.shi@mediatek.com)
    
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1673816
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Hsin-Yi Wang <hsinyi@chromium.org>
    
    FIXUP: FROMLIST: drm/panel: support for auo, kd101n80-45na wuxga dsi video mode panel
    
    Swap width/height in panel description, as this is a portrait
    panel.
    
    Reported here:
    https://patchwork.kernel.org/patch/11012583/#22723611
    
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1675026
    Reviewed-by: Hsin-Yi Wang <hsinyi@chromium.org>
    
    FIXUP: FROMLIST: drm/panel: support for auo, kd101n80-45na wuxga dsi video mode panel
    
    This fixup resolves differences between v2 and v4.
    
    v2: https://patchwork.kernel.org/patch/11012583
    v4: https://patchwork.kernel.org/patch/11088863
    
    BUG=b:135067265
    TEST=Boot krane
    
    Signed-off-by: Ryan Case <ryandcase@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1764338
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    
    Change-Id: I46b8b42762289809a46de8d73f03a2ad98517a10
    Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
    
    FIXUP: FROMLIST: drm/panel: support for auo, kd101n80-45na wuxga dsi video mode panel
    
    This fixup resolves differences between v4 and v7.
    
    v4: https://patchwork.kernel.org/patch/11088863
    v7: https://patchwork.kernel.org/patch/11186577
    
    BUG=b:132937157
    BUG=b:135067265
    TEST=Boot krane
    
    Change-Id: Idf4237b39fa69a6dcf77638ca0f5c7527f385be0
    Signed-off-by: Jitao Shi <jitao.shi@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1857839
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/gpu/drm/panel/panel-boe-tv101wum-nl6.c
+++ drivers/gpu/drm/panel/panel-boe-tv101wum-nl6.c
@@ -35,6 +35,7 @@ struct panel_desc {
 	enum mipi_dsi_pixel_format format;
 	const struct panel_init_cmd *init_cmds;
 	unsigned int lanes;
+	bool discharge_on_disable;
 };
 
 struct boe_panel {
@@ -372,6 +373,15 @@ static const struct panel_init_cmd boe_init_cmd[] = {
 	{},
 };
 
+static const struct panel_init_cmd auo_kd101n80_45na_init_cmd[] = {
+	_INIT_DELAY_CMD(24),
+	_INIT_DCS_CMD(0x11),
+	_INIT_DELAY_CMD(120),
+	_INIT_DCS_CMD(0x29),
+	_INIT_DELAY_CMD(120),
+	{},
+};
+
 static inline struct boe_panel *to_boe_panel(struct drm_panel *panel)
 {
 	return container_of(panel, struct boe_panel, base);
@@ -381,7 +391,7 @@ static int boe_panel_init(struct boe_panel *boe)
 {
 	struct mipi_dsi_device *dsi = boe->dsi;
 	struct drm_panel *panel = &boe->base;
-	int err, i;
+	int i, err = 0;
 
 	if (boe->desc->init_cmds) {
 		const struct panel_init_cmd *init_cmds = boe->desc->init_cmds;
@@ -401,6 +411,7 @@ static int boe_panel_init(struct boe_panel *boe)
 							 &cmd->data[1],
 							 cmd->len - 1);
 				break;
+
 			default:
 				err = -EINVAL;
 			}
@@ -451,19 +462,29 @@ static int boe_panel_unprepare(struct drm_panel *panel)
 	if (!boe->prepared)
 		return 0;
 
-	ret = boe_panel_off(boe);
-	if (ret < 0) {
-		dev_err(panel->dev, "failed to set panel off: %d\n", ret);
-		return ret;
+	if (boe->desc->discharge_on_disable) {
+		msleep(150);
+		regulator_disable(boe->avee);
+		regulator_disable(boe->avdd);
+		usleep_range(5000, 7000);
+		gpiod_set_value(boe->enable_gpio, 0);
+		usleep_range(5000, 7000);
+		regulator_disable(boe->pp1800);
+	} else {
+		ret = boe_panel_off(boe);
+		if (ret < 0) {
+			dev_err(panel->dev, "failed to set panel off: %d\n",
+				ret);
+			return ret;
+		}
+		msleep(150);
+		gpiod_set_value(boe->enable_gpio, 0);
+		usleep_range(500, 1000);
+		regulator_disable(boe->avee);
+		regulator_disable(boe->avdd);
+		usleep_range(5000, 7000);
+		regulator_disable(boe->pp1800);
 	}
-
-	msleep(150);
-	gpiod_set_value(boe->enable_gpio, 0);
-	usleep_range(500, 1000);
-	regulator_disable(boe->avee);
-	regulator_disable(boe->avdd);
-	usleep_range(5000, 7000);
-	regulator_disable(boe->pp1800);
 
 	boe->prepared = false;
 
@@ -494,10 +515,14 @@ static int boe_panel_prepare(struct drm_panel *panel)
 	if (ret < 0)
 		goto poweroffavdd;
 
-	msleep(100);
+	usleep_range(5000, 10000);
 
 	gpiod_set_value(boe->enable_gpio, 1);
-	usleep_range(10000, 12000);
+	usleep_range(1000, 2000);
+	gpiod_set_value(boe->enable_gpio, 0);
+	usleep_range(1000, 2000);
+	gpiod_set_value(boe->enable_gpio, 1);
+	usleep_range(6000, 10000);
 
 	ret = boe_panel_init(boe);
 	if (ret < 0) {
@@ -529,6 +554,8 @@ static int boe_panel_enable(struct drm_panel *panel)
 	if (boe->enabled)
 		return 0;
 
+	msleep(130);
+
 	ret = backlight_enable(boe->backlight);
 	if (ret) {
 		dev_err(panel->dev, "Failed to enable backlight %d\n",
@@ -541,7 +568,7 @@ static int boe_panel_enable(struct drm_panel *panel)
 	return 0;
 }
 
-static const struct drm_display_mode boe_default_mode = {
+static const struct drm_display_mode boe_tv101wum_nl6_default_mode = {
 	.clock = 159425,
 	.hdisplay = 1200,
 	.hsync_start = 1200 + 100,
@@ -555,7 +582,7 @@ static const struct drm_display_mode boe_default_mode = {
 };
 
 static const struct panel_desc boe_tv101wum_nl6_desc = {
-	.modes = &boe_default_mode,
+	.modes = &boe_tv101wum_nl6_default_mode,
 	.bpc = 8,
 	.size = {
 		.width_mm = 135,
@@ -566,6 +593,35 @@ static const struct panel_desc boe_tv101wum_nl6_desc = {
 	.mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
 		      MIPI_DSI_MODE_LPM,
 	.init_cmds = boe_init_cmd,
+	.discharge_on_disable = false,
+};
+
+static const struct drm_display_mode auo_kd101n80_45na_default_mode = {
+	.clock = 157000,
+	.hdisplay = 1200,
+	.hsync_start = 1200 + 80,
+	.hsync_end = 1200 + 80 + 24,
+	.htotal = 1200 + 80 + 24 + 36,
+	.vdisplay = 1920,
+	.vsync_start = 1920 + 16,
+	.vsync_end = 1920 + 16 + 4,
+	.vtotal = 1920 + 16 + 4 + 16,
+	.vrefresh = 60,
+};
+
+static const struct panel_desc auo_kd101n80_45na_desc = {
+	.modes = &auo_kd101n80_45na_default_mode,
+	.bpc = 8,
+	.size = {
+		.width_mm = 135,
+		.height_mm = 216,
+	},
+	.lanes = 4,
+	.format = MIPI_DSI_FMT_RGB888,
+	.mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
+		      MIPI_DSI_MODE_LPM,
+	.init_cmds = auo_kd101n80_45na_init_cmd,
+	.discharge_on_disable = true,
 };
 
 static int boe_panel_get_modes(struct drm_panel *panel)
@@ -696,6 +752,9 @@ static void boe_panel_shutdown(struct mipi_dsi_device *dsi)
 static const struct of_device_id boe_of_match[] = {
 	{ .compatible = "boe,tv101wum-nl6",
 	  .data = &boe_tv101wum_nl6_desc
+	},
+	{ .compatible = "auo,kd101n80-45na",
+	  .data = &auo_kd101n80_45na_desc
 	},
 	{ /* sentinel */ }
 };
