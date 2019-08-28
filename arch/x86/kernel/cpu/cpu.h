a103a32ac6a2c8bbb8fb217af92c9f6bd08e138c
Nick Desaulniers <ndesaulniers@google.com>
2019-08-28 15:55:31 -0700

    x86: prefer __section, __maybe_unused and __aligned from compiler_attributes.h
    GCC unescapes escaped string section names while Clang does not. Because
    __section uses the `#` stringification operator for the section name, it
    doesn't need to be escaped.
    
    Instead, we should:
    1. Prefer __section(.section_name_no_quotes).
    2. Only use __attribute__((__section__(".section"))) when creating the
    section name via C preprocessor (see the definition of __define_initcall
    in arch/um/include/shared/init.h).
    
    This antipattern was found with:
    $ grep -e __section\(\" -e __section__\(\" -r
    
    See the discussions in:
    Link: https://bugs.llvm.org/show_bug.cgi?id=42950
    Link: https://marc.info/?l=linux-netdev&m=156412960619946&w=2
    Link: https://github.com/ClangBuiltLinux/linux/issues/619
    Reported-by: Sedat Dilek <sedat.dilek@gmail.com>
    Suggested-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Tested-by: Sedat Dilek <sedat.dilek@gmail.com>
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    [Added back "unused" as __maybe_unused]
    [Changed title to reflect the other changes]
    Signed-off-by: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>

--- arch/x86/kernel/cpu/cpu.h
+++ arch/x86/kernel/cpu/cpu.h
@@ -38,8 +38,7 @@ struct _tlb_table {
 
 #define cpu_dev_register(cpu_devX) \
 	static const struct cpu_dev *const __cpu_dev_##cpu_devX __used \
-	__attribute__((__section__(".x86_cpu_dev.init"))) = \
-	&cpu_devX;
+	__section(.x86_cpu_dev.init) = &cpu_devX;
 
 extern const struct cpu_dev *const __x86_cpu_dev_start[],
 			    *const __x86_cpu_dev_end[];
