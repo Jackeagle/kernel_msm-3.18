d14728f4948f34a427ea7f0746a70c79cb97df70
Takashi Iwai <tiwai@suse.de>
2019-11-05 09:01:35 +0100

    ALSA: memalloc: Allow NULL device for SNDRV_DMA_TYPE_CONTINUOUS type
    Currently we pass the artificial device pointer to the allocation
    helper in the case of SNDRV_DMA_TYPE_CONTINUOUS for passing the GFP
    flags.  But all common cases are the allocations with GFP_KERNEL, and
    it's messy to put this in each place.
    
    In this patch, the memalloc core helper is changed to accept the NULL
    device pointer and it treats as the default mode, GFP_KERNEL, so that
    all callers can omit the complex argument but just leave NULL.
    
    Link: https://lore.kernel.org/r/20191105080138.1260-2-tiwai@suse.de
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/memalloc.c
+++ sound/core/memalloc.c
@@ -99,6 +99,13 @@ static void snd_free_dev_iram(struct snd_dma_buffer *dmab)
  *
  */
 
+static inline gfp_t snd_mem_get_gfp_flags(const struct device *dev)
+{
+	if (!dev)
+		return GFP_KERNEL;
+	else
+		return (__force gfp_t)(unsigned long)dev;
+}
 
 /**
  * snd_dma_alloc_pages - allocate the buffer area according to the given type
@@ -129,7 +136,7 @@ int snd_dma_alloc_pages(int type, struct device *device, size_t size,
 	switch (type) {
 	case SNDRV_DMA_TYPE_CONTINUOUS:
 		dmab->area = alloc_pages_exact(size,
-					       (__force gfp_t)(unsigned long)device);
+					       snd_mem_get_gfp_flags(device));
 		dmab->addr = 0;
 		break;
 #ifdef CONFIG_HAS_DMA
