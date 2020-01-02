9df57681103e2a85b67a1d1f95366dd21ae9b5ac
Esben Haabendal <esben@geanix.com>
2020-01-02 14:08:25 +0100

    mtd: rawnand: gpmi: Fix suspend/resume problem
    On system resume, the gpmi clock must be enabled before accessing gpmi
    block.  Without this, resume causes something like
    
    [  661.348790] gpmi_reset_block(5cbb0f7e): module reset timeout
    [  661.348889] gpmi-nand 1806000.gpmi-nand: Error setting GPMI : -110
    [  661.348928] PM: dpm_run_callback(): platform_pm_resume+0x0/0x44 returns -110
    [  661.348961] PM: Device 1806000.gpmi-nand failed to resume: error -110
    
    Signed-off-by: Esben Haabendal <esben@geanix.com>
    Acked-by: Han Xu <han.xu@nxp.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/nand/raw/gpmi-nand/gpmi-nand.c
+++ drivers/mtd/nand/raw/gpmi-nand/gpmi-nand.c
@@ -148,6 +148,10 @@ static int gpmi_init(struct gpmi_nand_data *this)
 	struct resources *r = &this->resources;
 	int ret;
 
+	ret = pm_runtime_get_sync(this->dev);
+	if (ret < 0)
+		return ret;
+
 	ret = gpmi_reset_block(r->gpmi_regs, false);
 	if (ret)
 		goto err_out;
@@ -181,6 +185,8 @@ static int gpmi_init(struct gpmi_nand_data *this)
 
 	return 0;
 err_out:
+	pm_runtime_mark_last_busy(this->dev);
+	pm_runtime_put_autosuspend(this->dev);
 	return ret;
 }
 
