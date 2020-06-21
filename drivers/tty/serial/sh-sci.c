a05f0b4611aa2d8db2c9bab5ccfe91b39890d561
Stephen Rothwell <sfr@canb.auug.org.au>
2020-06-22 00:30:06 +1000

    Revert "drivers/tty/serial/sh-sci.c: suppress uninitialized var warning"
    This reverts commit a1e988dda7bb37a9cada7ccd55de32da98fca7d8.

--- drivers/tty/serial/sh-sci.c
+++ drivers/tty/serial/sh-sci.c
@@ -1352,7 +1352,7 @@ static int sci_dma_rx_submit(struct sci_port *s, bool port_lock_held)
 {
 	struct dma_chan *chan = s->chan_rx;
 	struct uart_port *port = &s->port;
-	unsigned long uninitialized_var(flags);
+	unsigned long flags;
 	int i;
 
 	for (i = 0; i < 2; i++) {
