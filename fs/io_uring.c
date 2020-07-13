4c8a37e212a8e8be52bfde279b642e761d9782db
Pavel Begunkov <asml.silence@gmail.com>
2020-07-13 22:59:20 +0300

    io_uring: follow **iovec idiom in io_import_iovec
    As for import_iovec(), return !=NULL iovec from io_import_iovec() only
    when it should be freed. That includes returning NULL when iovec is
    already in req->io, because it should be deallocated by other means,
    e.g. inside op handler. After io_setup_async_rw() local iovec to ->io,
    just mark it NULL, to follow the idea in io_{read,write} as well.
    
    That's easier to follow, and especially useful if we want to reuse
    per-op space for completion data.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    [axboe: only call kfree() on non-NULL pointer]
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2739,10 +2739,8 @@ static ssize_t io_import_iovec(int rw, struct io_kiocb *req,
 	if (req->io) {
 		struct io_async_rw *iorw = &req->io->rw;
 
-		*iovec = iorw->iov;
-		iov_iter_init(iter, rw, *iovec, iorw->nr_segs, iorw->size);
-		if (iorw->iov == iorw->fast_iov)
-			*iovec = NULL;
+		iov_iter_init(iter, rw, iorw->iov, iorw->nr_segs, iorw->size);
+		*iovec = NULL;
 		return iorw->size;
 	}
 
@@ -3025,6 +3023,8 @@ copy_iov:
 						inline_vecs, &iter);
 			if (ret)
 				goto out_free;
+			/* it's copied and will be cleaned with ->io */
+			iovec = NULL;
 			/* if we can retry, do so with the callbacks armed */
 			if (io_rw_should_retry(req)) {
 				ret2 = io_iter_do_read(req, &iter);
@@ -3040,7 +3040,7 @@ copy_iov:
 		}
 	}
 out_free:
-	if (!(req->flags & REQ_F_NEED_CLEANUP))
+	if (iovec)
 		kfree(iovec);
 	return ret;
 }
@@ -3142,11 +3142,13 @@ copy_iov:
 						inline_vecs, &iter);
 			if (ret)
 				goto out_free;
+			/* it's copied and will be cleaned with ->io */
+			iovec = NULL;
 			return -EAGAIN;
 		}
 	}
 out_free:
-	if (!(req->flags & REQ_F_NEED_CLEANUP))
+	if (iovec)
 		kfree(iovec);
 	return ret;
 }
